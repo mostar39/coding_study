6a08d1b715ac9abb2c65cbe0750efd02d2346f16
mostar39 <mostar39@naver.com>
2022-02-14 14:20:45 +0900

    .

---
+++ 5_chapter/5.2_book.py
@@ -0,0 +1,17 @@
+#Queue : FIFO(First In First Out)
+from collections import deque
+
+queue = deque()
+
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+
+print(queue)
+queue.reverse()
+print(queue)
---
+++ 5_chapter/5.3_book.py
@@ -0,0 +1,8 @@
+#재귀 함수
+
+# error :
+# RecursionError : Maximum recursion depth exceeded while pickling an object
+
+def recursive_function() :
+	print('재귀 함수 호출')
+	recursive_function()
